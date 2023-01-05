47b98dd61707e376df9f50879f420a9d7f7386ca
sergehall <sergioartg@gmail.com>
2023-01-05 03:27:03 -0800

    change time demon sent mail

--- src/users/infrastructure/users.repository.ts
+++ src/users/infrastructure/users.repository.ts
@@ -22,11 +22,8 @@ export class UsersRepository {
   async userAlreadyExist(login: string, email: string): Promise<string | null> {
     const findLogin = await this.UsersModel.findOne({ login: { $eq: login } });
     const findEmail = await this.UsersModel.findOne({ email: { $eq: email } });
-    if (findLogin) {
-      return 'login';
-    }
-    if (findEmail) {
-      return 'email';
+    if (findLogin || findEmail) {
+      return findLogin ? 'login' : 'email';
     }
     return null;
   }
