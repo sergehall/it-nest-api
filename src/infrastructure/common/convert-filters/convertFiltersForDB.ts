5b266a0776311c35fcccf5ca6b5994ba71e785b3
sergehall <sergioartg@gmail.com>
2023-01-24 04:30:10 -0800

    refactoring ConvertFiltersForDB

--- src/infrastructure/common/convert-filters/convertFiltersForDB.ts
+++ src/infrastructure/common/convert-filters/convertFiltersForDB.ts
@@ -1,32 +1,60 @@
 import { Injectable } from '@nestjs/common';
-import { PathFilterEnum } from './enums/filters.enums';
-import { ConvertFilterType, QueryArrType } from './types/convert-filter.types';
+import {
+  PathFilterEnum,
+  PatternConvertFilterType,
+} from './enums/filters.enums';
+import { QueryArrType } from './types/convert-filter.types';
 
 @Injectable()
 export class ConvertFiltersForDB {
-  async convert([...rawFilters]: QueryArrType) {
-    return this._forMongo([...rawFilters], PathFilterEnum);
+  async convert(queryFilters: QueryArrType) {
+    return this._forMongo(queryFilters, PathFilterEnum);
   }
 
-  async _forMongo([...rawFilters], pattern: ConvertFilterType) {
+  async _forMongo([...queryFilters], pathFilterEnum: PatternConvertFilterType) {
     const convertedFilters = [];
-    for (let i = 0, l = Object.keys(rawFilters).length; i < l; i++) {
-      for (const key in rawFilters[i]) {
-        if (pattern.hasOwnProperty(key) && rawFilters[i][key].length !== 0) {
-          const convertedFilter = {};
-          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-          // @ts-ignore
-          convertedFilter[pattern[key]] = {
-            $regex: rawFilters[i][key].toLowerCase(),
-            $options: 'i',
-          };
-          convertedFilters.push(convertedFilter);
+    try {
+      for (let i = 0, l = Object.keys(queryFilters).length; i < l; i++) {
+        for (const key in queryFilters[i]) {
+          if (pathFilterEnum.hasOwnProperty(key) && queryFilters[i][key]) {
+            const convertedFilter = {};
+            if (key === 'banStatus' && queryFilters[i][key] === 'true') {
+              queryFilters[i][key] = true;
+              // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+              // @ts-ignore
+              convertedFilter[pathFilterEnum[key]] = {
+                $eq: queryFilters[i][key],
+              };
+              convertedFilters.push(convertedFilter);
+              continue;
+            }
+            if (key === 'banStatus' && queryFilters[i][key] === 'false') {
+              queryFilters[i][key] = false;
+              // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+              // @ts-ignore
+              convertedFilter[pathFilterEnum[key]] = {
+                $eq: queryFilters[i][key],
+              };
+              convertedFilters.push(convertedFilter);
+              continue;
+            }
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            convertedFilter[pathFilterEnum[key]] = {
+              $regex: queryFilters[i][key].toLowerCase(),
+              $options: 'i',
+            };
+            convertedFilters.push(convertedFilter);
+          }
         }
       }
+      if (convertedFilters.length === 0) {
+        convertedFilters.push({});
+      }
+      return convertedFilters;
+    } catch (err) {
+      console.log(err);
+      return [{}];
     }
-    if (convertedFilters.length === 0) {
-      convertedFilters.push({});
-    }
-    return convertedFilters;
   }
 }
--- src/infrastructure/common/convert-filters/enums/filters.enums.ts
+++ src/infrastructure/common/convert-filters/enums/filters.enums.ts
@@ -3,4 +3,14 @@ export enum PathFilterEnum {
   searchLoginTerm = 'login',
   searchEmailTerm = 'email',
   blogId = 'blogId',
+  userId = 'blogOwnerInfo.userId',
+  banStatus = 'banInfo.isBanned',
 }
+export type PatternConvertFilterType = {
+  searchNameTerm: string;
+  searchLoginTerm: string;
+  searchEmailTerm: string;
+  blogId: string;
+  userId: string;
+  banStatus: string;
+};
